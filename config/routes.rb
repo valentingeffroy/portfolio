02706434b8e2b6cc42ae4a9a4ff9f41177164ea1
valentin geffroy <geffroy.valentin@gmail.com>
2019-12-17 16:50:19 +0100

    navbar + banner + footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/contact', to: 'pages#about', as: :about
+  get 'pages/about', to: 'pages#contact', as: :contact
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
